59f666ae11cd31d671e33d347206bda04858eca2
CervinoB <101291672+CervinoB@users.noreply.github.com>
2025-02-01 23:35:22 -0300

    initial commmit

---
+++ lib/consts.go
@@ -0,0 +1,32 @@
+// Package consts houses some constants needed across k6
+package consts
+
+import (
+	"strings"
+)
+
+// Version contains the current semantic version of k6.
+const Version = "0.0.0"
+
+// Banner returns the ASCII-art banner with the k6 logo
+func Banner() string {
+	banner := strings.Join([]string{
+		`              _`,
+		`             | |`,
+		`             | |===( )   //////`,
+		`             |_|   |||  | o o|`,
+		`                    ||| ( c  )                  ____`,
+		`                     ||| \= /                  ||   \_`,
+		`                      ||||||                   ||     |`,
+		`                      ||||||                ...||__/|-"`,
+		`                      ||||||             __|________|__`,
+		`                        |||             |______________|`,
+		`                        |||             || ||      || ||`,
+		`                        |||             || ||      || ||`,
+		`------------------------|||-------------||-||------||-||-------`,
+		`                        |__>            || ||      || ||`,
+		`                                                        .jcerv`,
+	}, "\n")
+
+	return banner
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 Joao Cervino jcervinobarbosa@gmail.com
+
+*/
+package main
+
+import "sonarcli/cmd"
+
+func main() {
+	cmd.Execute()
+}
