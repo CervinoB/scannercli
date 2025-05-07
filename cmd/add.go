fbdc0911f982c3c22f099e17b7eda7bcd93deb49
CervinoB <jcervinobarbosa@gmail.com>
2025-05-06 21:31:43 -0300

    add add command and write args to a file

---
+++ cmd/add.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2025 Joao Cervino jcervinobarbosa@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/CervinoB/scannercli/internal/todo"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: addRun,
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	fmt.Println("add called")
+
+	items := []todo.Item{}
+
+	for _, x := range args {
+		items = append(items, todo.Item{Text: x})
+	}
+	err := todo.SaveItems(".tridos.json", items)
+	if err != nil {
+		fmt.Errorf("%v", err)
+	}
+	// fmt.Printf("%#v\n", items)
+
+}
---
+++ cmd/scan.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2025 Joao Cervino jcervinobarbosa@gmail.com
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// scanCmd represents the scan command
+var scanCmd = &cobra.Command{
+	Use:   "scan",
+	Short: "Scan repositories with the scanner",
+	Long: `The scan command allows you to scan repositories using the scanner tool.
+It provides detailed analysis and insights for the specified repositories.`,
+	Run: scanRun,
+}
+
+func init() {
+	rootCmd.AddCommand(scanCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// scanCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// scanCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func scanRun(cmd *cobra.Command, args []string) {
+	fmt.Println("scan called")
+}
